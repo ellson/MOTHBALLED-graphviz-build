56c224c9546a34e25faca6bdf126f3facf047f29
glenlow <devnull@localhost>
2008-05-03 14:14:58 +0000

    clean up working directory

--- macosx/graphviz-bin-pkg.sh
+++ macosx/graphviz-bin-pkg.sh
@@ -4,6 +4,7 @@
 graphviz_host=www.graphviz.org
 graphviz_path=/pub/graphviz
 path=$graphviz_path/$1
+work=$HOME/tmp/gviz
 
 # search for last graphviz tarball in the public sources
 source=`ssh gviz@www.graphviz.org ls -r $path | sed -n 's/^\(graphviz-[0-9.]*\).tar.gz$/\1/p
@@ -14,8 +15,17 @@ q'`
 
 if test -n $source
 then
+	# clean up previous builds
+	rm -rf $work/*
+	cd $work
+
+	# get the sources
 	scp gviz@$graphviz_host:$path/$source.tar.gz .
+	
+	# build the package
 	tar xzf $source.tar.gz
 	make -C $source/macosx/build
+
+	# put the package
 	scp $source/macosx/build/graphviz-*.pkg gviz@$graphviz_host:$path/
 fi
