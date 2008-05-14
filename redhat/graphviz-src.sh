86b52b3a05502ae270d1846b62d09b437db045c6
ellson <devnull@localhost>
2008-05-14 19:09:47 +0000

    typo

--- redhat/graphviz-src.sh
+++ redhat/graphviz-src.sh
@@ -66,6 +66,7 @@ if test $SRCDIR = CURRENT; then
     VERSION_MICRO=$DATE.att
 else
     VERSION_MICRO=att
+fi
 
 sed "s/\(m4_define(graphviz_version_micro, \)[0-9.]*)/\1$VERSION_MICRO)/" <configure.ac >t$$
 mv t$$ configure.ac
