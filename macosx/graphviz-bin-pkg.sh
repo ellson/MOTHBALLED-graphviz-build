a3846aaec086520ce1824260f704b3b0f692e201
ellson <devnull@localhost>
2008-08-20 21:18:28 +0000

    fixing macos build for directory changes

--- macosx/graphviz-bin-pkg.sh
+++ macosx/graphviz-bin-pkg.sh
@@ -4,20 +4,23 @@
 
 # where everything is
 graphviz_host=www.graphviz.org
-graphviz_path=/pub/graphviz
+graphviz_path=/pub/graphviz/
 
 if test -z $1; then
-    path=$graphviz_path/CURRENT
+    path=$graphviz_path/development
 else
-    path=$graphviz_path/$1
+    path=$graphviz_path/stable
 fi
 
 work=$HOME/tmp/gviz
 fix=$HOME/fix
 
+SOURCES=$path/SOURCES
+PKGS=$path/macos
+
 # search for last graphviz tarball in the public sources
 source=
-for file in `ssh gviz@www.graphviz.org ls -t $path`; do
+for file in `ssh gviz@www.graphviz.org ls -t $SOURCES`; do
         source=`expr $file : '\(graphviz-[0-9.]*\).tar.gz$'`
         if test -n "$source"; then
                 break
@@ -34,12 +37,12 @@ then
 	cd $work
 
 	# get the sources
-	scp gviz@$graphviz_host:$path/$source.tar.gz . 2>$LOG
+	scp gviz@$graphviz_host:$SOURCES/$source.tar.gz . 2>$LOG
 	
 	# build the package
 	tar xzf $source.tar.gz
 	make -C $source/macosx/build PREBUILD=$fix >>$LOG 2>&1
 
 	# put the package
-	scp $source/macosx/build/graphviz.pkg gviz@$graphviz_host:$path/$source.pkg 2>>$LOG
+	scp $source/macosx/build/graphviz.pkg gviz@$graphviz_host:$PKGS/$source.pkg 2>>$LOG
 fi
