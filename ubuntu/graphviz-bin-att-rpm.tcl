260dfbe3ba930ffe06b977eacab313d1a39b451e
ellson <devnull@localhost>
2008-08-25 17:21:44 +0000

    starting ubuntu build scripts from rehat scripts

---
+++ ubuntu/graphviz-bin-att-rpm.tcl
@@ -0,0 +1,13 @@
+#!/usr/bin/tclsh
+
+# $Id$ $Revision$
+
+set own att
+
+source [file dirname $argv0]/build1.common
+
+set pkg graphviz
+set isnoarch 0
+
+source [file dirname $argv0]/build2.common
+source [file dirname $argv0]/build3.common
