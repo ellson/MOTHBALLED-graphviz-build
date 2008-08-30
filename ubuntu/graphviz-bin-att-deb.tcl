847895b3bf9e9ab1c24451a5d995dabb122b018b
ellson <devnull@localhost>
2008-08-30 23:09:26 +0000

    ubuntu build scripts

--- ubuntu/graphviz-bin-att-deb.tcl
+++ ubuntu/graphviz-bin-att-deb.tcl
@@ -2,18 +2,11 @@
 
 # $Id$ $Revision$
 
-set own ""
+set own att
 
 source [file dirname $argv0]/build1.common
-
-set pkg webdot
-set isnoarch 1
-
-source [file dirname $argv0]/build2.common
 
 set pkg graphviz
 set isnoarch 0
 
 source [file dirname $argv0]/build2.common
-
-source [file dirname $argv0]/build3.common
--- ubuntu/graphviz-bin-deb.tcl
+++ ubuntu/graphviz-bin-deb.tcl
@@ -2,12 +2,16 @@
 
 # $Id$ $Revision$
 
-set own att
+set own ""
 
 source [file dirname $argv0]/build1.common
+
+#set pkg webdot
+#set isnoarch 1
+#
+#source [file dirname $argv0]/build2.common
 
 set pkg graphviz
 set isnoarch 0
 
 source [file dirname $argv0]/build2.common
-source [file dirname $argv0]/build3.common
