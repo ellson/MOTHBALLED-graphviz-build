34ae6928c8eb711b346671d3d65af115f001531f
ellson <devnull@localhost>
2009-06-13 15:16:56 +0000

    split out part of the ubuntu build scripts to make room for ubuntu-version customization

--- ubuntu/graphviz-bin-att-deb.tcl
+++ ubuntu/graphviz-bin-att-deb.tcl
@@ -10,3 +10,4 @@ set pkg graphviz
 set isnoarch 0
 
 source [file dirname $argv0]/build2.common
+source [file dirname $argv0]/build3.common
--- ubuntu/graphviz-bin-deb.tcl
+++ ubuntu/graphviz-bin-deb.tcl
@@ -15,3 +15,4 @@ set pkg graphviz
 set isnoarch 0
 
 source [file dirname $argv0]/build2.common
+source [file dirname $argv0]/build3.common
