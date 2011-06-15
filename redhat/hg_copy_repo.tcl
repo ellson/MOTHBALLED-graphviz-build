bdbc114a369a397caeb5bee24f9611a9f38bfd9f
dperry <devnull@localhost>
2011-06-15 20:38:22 +0000

    Mercurial replacement for anoncvs.tcl

---
+++ redhat/hg_copy_repo.tcl
@@ -0,0 +1,18 @@
+#!/bin/env expect
+  
+if {$argc == 0} {
+        send_user "Usage: $argv0 repository_name\n"
+        exit
+}
+set timeout 300 
+foreach {a1} $argv {break}
+ 
+spawn ~/graphviz-build/bin/hg_copy_repo $a1
+while {1} {
+        expect timeout {
+                send_user "Failed to checkout $a1 from mercurial server (timeout)\n"
+                exit
+        } eof {
+                break
+        }
+}
