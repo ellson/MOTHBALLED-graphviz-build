bca44cc89619c1a2e5d77195f10e056d7ca25cb1
dperry <devnull@localhost>
2011-06-15 18:05:00 +0000

    Copy mercurial repo tarball from hg.

---
+++ bin/hg_tar_repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+[ $# -eq 0 ] && { echo "Usage $0 hg_copy_repo repo_name"; exit 999; }
+
+base=$1
+
+SERVER="hg.research.att.com"
+USR="gviz"
+OUT="outfile_$base"
+ssh $USR@$SERVER "(bin/hg_get_repo ${base})" > $OUT 2>&1
+scp -r $USR@$SERVER:${base}.tgz . >> $OUT 2>&1
+
