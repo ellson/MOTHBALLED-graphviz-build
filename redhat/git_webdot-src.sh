ff4323d834ff0425488df66082e471ab33d868c4
John Ellson <john.ellson@comcast.net>
2013-09-22 17:49:14 -0400

    remove zip files

--- redhat/git_webdot-src.sh
+++ redhat/git_webdot-src.sh
@@ -27,16 +27,14 @@ RPMBUILD=$HOME/rpmbuild/$HOST
 cd $HOME/tmp/gviz
 
 # cleanup previous build
-rm -rf webdot webdot-master
-
+rm -rf webdot webdot-master master.zip*
 
 # # obtain latest from mercurial
 # $HOME/graphviz-build/bin/git_copy_repo.sh webdot
 
 wget https://github.com/ellson/webdot/archive/master.zip
 unzip -q master.zip
-
-
+rm -rf master.zip*
 
 #cd webdot
 cd webdot-master
