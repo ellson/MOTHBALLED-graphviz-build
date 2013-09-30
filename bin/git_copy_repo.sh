723bf53c92fa0c77430420ad9c601110b867b720
John Ellson <john.ellson@comcast.net>
2013-09-30 14:07:14 -0400

    some weird wget on terra doesn't leave .zip extent

--- bin/git_copy_repo.sh
+++ bin/git_copy_repo.sh
@@ -3,6 +3,16 @@
 
 rm -f master.zip*
 wget https://github.com/ellson/$1/archive/master.zip
-unzip master.zip
-(rm -rf $1 master.zip;mv ${1}-master $1)
+if test -f master.zip; then
+	unzip master.zip
+	rm master.zip
+else
+	if test -f master; then
+		unzip master
+		rm master
+	else
+		echo "Failed to transfer master.zip"; exit 1
+	fi
+fi
+(rm -rf $1;mv ${1}-master $1)
 
