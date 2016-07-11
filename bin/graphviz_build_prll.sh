743b67c8fc8e809ec1d22a4e409f8d96ffe70a6f
John Ellson <ellson@research.att.com>
2016-07-11 15:25:34 -0400

    update builders

--- bin/graphviz_build_prll.sh
+++ bin/graphviz_build_prll.sh
@@ -4,17 +4,17 @@
 #     DIR=CURRENT (or empty) to build the current snapshot
 DIR=$1
 
-BUILD_HOSTS="fc20-64 fc20 centos6-64 centos6 centos5-64 centos5"
-parallel -j 2 --arg-sep ::: ssh -x {} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
+BUILD_HOSTS="bld-centos7 bld-centos7-32 bld-centos6 bld-centos6-32 bld-centos5 bld-centos5-32"
+parallel -j 2 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
 
-BUILD_HOSTS="ubuntu12 ubuntu12-64 ubuntu13 ubuntu13-64"
-parallel -j 2 --arg-sep ::: ssh -x {} graphviz-build/ubuntu/graphviz-bin-deb.tcl $DIR ::: $BUILD_HOSTS
+#BUILD_HOSTS="ubuntu12 ubuntu12-64 ubuntu13 ubuntu13-64"
+#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/ubuntu/graphviz-bin-deb.tcl $DIR ::: $BUILD_HOSTS
 
-BUILD_HOSTS="snares"
-parallel -j 2 --arg-sep ::: ssh -x {} graphviz-build/macosx/graphviz-snowleopard-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+#BUILD_HOSTS="snares"
+#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-snowleopard-bin-pkg.sh $DIR ::: $BUILD_HOSTS
 
 BUILD_HOSTS="pome"
-parallel -j 2 --arg-sep ::: ssh -x {} graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
 
 BUILD_HOSTS="empire"
-parallel -j 2 --arg-sep ::: ssh -x {} graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
