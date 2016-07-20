4288f7ecf757cf97709f97b7a293d57e33caebc8
John Ellson <ellson@research.att.com>
2016-07-20 10:39:46 -0400

    move fedora builds

--- bin/graphviz_build_prll.sh
+++ bin/graphviz_build_prll.sh
@@ -4,8 +4,24 @@
 #     DIR=CURRENT (or empty) to build the current snapshot
 DIR=$1
 
-BUILD_HOSTS="bld-centos7 bld-centos7-32 bld-centos6 bld-centos6-32 bld-centos5 bld-centos5-32"
-parallel -j 2 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
+BUILD_HOSTS="bld-centos5
+        bld-centos6
+        bld-centos7
+        bld-centos5-32
+        bld-centos6-32
+        bld-centos7-32
+        bld-fedora21
+        bld-fedora22
+        bld-fedora23
+        bld-fedora24
+        bld-fedora25
+        bld-fedora21-32
+        bld-fedora22-32
+        bld-fedora23-32
+        bld-fedora24-32
+        bld-fedora25-32"
+
+parallel -j 4 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
 
 #BUILD_HOSTS="ubuntu12 ubuntu12-64 ubuntu13 ubuntu13-64"
 #parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/ubuntu/graphviz-bin-deb.tcl $DIR ::: $BUILD_HOSTS
