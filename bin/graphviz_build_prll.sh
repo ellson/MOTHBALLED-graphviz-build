cfb3d5a19011721444413e5fab701dee541884c2
John Ellson <ellson@research.att.com>
2016-08-01 09:49:33 -0400

    switch to pssh

--- bin/graphviz_build_prll.sh
+++ bin/graphviz_build_prll.sh
@@ -4,24 +4,32 @@
 #     DIR=CURRENT (or empty) to build the current snapshot
 DIR=$1
 
-BUILD_HOSTS="bld-centos5
-        bld-centos6
-        bld-centos7
-        bld-centos5-32
-        bld-centos6-32
-        bld-centos7-32
-        bld-fedora21
-        bld-fedora22
-        bld-fedora23
-        bld-fedora24
-        bld-fedora25
-        bld-fedora21-32
-        bld-fedora22-32
-        bld-fedora23-32
-        bld-fedora24-32
-        bld-fedora25-32"
+BUILD_HOSTS="
+	gviz@bld-centos5
+        gviz@bld-centos6
+        gviz@bld-centos7
+        gviz@bld-centos5-32
+        gviz@bld-centos6-32
+        gviz@bld-centos7-32
+        gviz@bld-fedora21
+        gviz@bld-fedora22
+        gviz@bld-fedora23
+        gviz@bld-fedora24
+        gviz@bld-fedora25
+        gviz@bld-fedora21-32
+        gviz@bld-fedora22-32
+        gviz@bld-fedora23-32
+        gviz@bld-fedora24-32
+        gviz@bld-fedora25-32
+"
 
-parallel -j 4 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
+ERR=graphviz_build_err
+OUT=graphviz_build_out
+rm -rf $ERR $OUT
+
+pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -p 4 PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR
+
+#parallel -j 4 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
 
 #BUILD_HOSTS="ubuntu12 ubuntu12-64 ubuntu13 ubuntu13-64"
 #parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/ubuntu/graphviz-bin-deb.tcl $DIR ::: $BUILD_HOSTS
@@ -29,8 +37,18 @@ parallel -j 4 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-bui
 #BUILD_HOSTS="snares"
 #parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-snowleopard-bin-pkg.sh $DIR ::: $BUILD_HOSTS
 
-BUILD_HOSTS="pome"
-parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+BUILD_HOSTS="
+	gviz@pome
+"
 
-BUILD_HOSTS="empire"
-parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -p 4 graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR
+
+#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+
+BUILD_HOSTS="
+	gviz@empire
+"
+
+pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -p 4 graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR
+
+#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
