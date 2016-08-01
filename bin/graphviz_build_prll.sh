e127dc780035861c270ee67ee1f5ae12b39bb34e
John Ellson <ellson@research.att.com>
2016-08-01 09:59:40 -0400

    increase timeout to 20min

--- bin/graphviz_build_prll.sh
+++ bin/graphviz_build_prll.sh
@@ -3,6 +3,9 @@
 # set DIR=ARCHIVE to build stable release
 #     DIR=CURRENT (or empty) to build the current snapshot
 DIR=$1
+ERR=graphviz_build_err
+OUT=graphviz_build_out
+rm -rf $ERR $OUT
 
 BUILD_HOSTS="
 	gviz@bld-centos5
@@ -22,33 +25,14 @@ BUILD_HOSTS="
         gviz@bld-fedora24-32
         gviz@bld-fedora25-32
 "
-
-ERR=graphviz_build_err
-OUT=graphviz_build_out
-rm -rf $ERR $OUT
-
-pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -p 4 PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR
-
-#parallel -j 4 --arg-sep ::: ssh -x gviz@{} PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR ::: $BUILD_HOSTS
-
-#BUILD_HOSTS="ubuntu12 ubuntu12-64 ubuntu13 ubuntu13-64"
-#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/ubuntu/graphviz-bin-deb.tcl $DIR ::: $BUILD_HOSTS
-
-#BUILD_HOSTS="snares"
-#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-snowleopard-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -t 12000 -p 4 PATH=/usr/bin:/bin:$PATH graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR
 
 BUILD_HOSTS="
 	gviz@pome
 "
-
-pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -p 4 graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR
-
-#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -t 12000 -p 4 graphviz-build/macosx/graphviz-mountainlion-bin-pkg.sh $DIR
 
 BUILD_HOSTS="
 	gviz@empire
 "
-
-pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -p 4 graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR
-
-#parallel -j 2 --arg-sep ::: ssh -x gviz@{} graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR ::: $BUILD_HOSTS
+pssh -H "$BUILD_HOSTS" -o $OUT -e $ERR -t 12000 -p 4 graphviz-build/macosx/graphviz-lion-bin-pkg.sh $DIR
