11b6ae7ca9410396f2b1631119359a60dabe15d7
John Ellson <ellson@research.att.com>
2017-07-26 10:21:57 -0400

    master graphviz-build from controlling host

--- bin/graphviz_build_prll.sh
+++ bin/graphviz_build_prll.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 # set DIR=ARCHIVE to build stable release
-#     DIR=CURRENT (or empty) to build the current snapshot
+#	 DIR=CURRENT (or empty) to build the current snapshot
 DIR=$1
 ERR=graphviz_build_err
 OUT=graphviz_build_out
 rm -rf $ERR $OUT
 
-BUILD_HOSTS="bld-centos6 bld-centos6-32 bld-centos7 bld-centos7-32 bld-fedora23 bld-fedora23-32 bld-fedora24 bld-fedora24-32 bld-fedora25 bld-fedora25-32 bld-fedora26 bld-fedora26-32 bld-fedora27 bld-fedora27-32"
+BUILD_HOSTS="
+	bld-centos6
+	bld-centos6-32
+	bld-centos7
+	bld-centos7-32
+	bld-fedora23
+	bld-fedora23-32
+	bld-fedora24
+	bld-fedora24-32
+	bld-fedora25
+	bld-fedora25-32
+	bld-fedora26
+	bld-fedora26-32
+	bld-fedora27
+	bld-fedora27-32
+"
 
 for h in $BUILD_HOSTS; do
-	rsync -av --exclude 'graphviz-build/.git' graphviz-build gviz@bld-centos7:
+	rsync -av --exclude 'graphviz-build/.git' graphviz-build gviz@$h:
 done
 
 pssh -H "$BUILD_HOSTS" -l gviz -o $OUT -e $ERR -t 12000 -p 4 graphviz-build/redhat/graphviz-bin-rpm.tcl $DIR
